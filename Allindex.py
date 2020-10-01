ce74285e1b4d5599f95582ee51c4811709374aa3
Goutam Tiwari <63529618+goutam980@users.noreply.github.com>
2020-10-01 01:40:47 -0700

    correct spelling and make changes in indent

--- Allindex.py
+++ Allindex.py
@@ -1,9 +1,11 @@
-print('program to print indices of all occurence of a given element in a given list\n Enetr a list')
+print('program to print indices of all occurence of a given element in a given list\n please Enetr a list')
 l=eval(input())
 e=eval(input('enter a element'))
 i=0
+#this is list to get element value
 print(e,"is at index :-",end=' ')
 for x in l:
+    
     if x==e:
         print(l.index(x,i),end=' ')
         i+=1
